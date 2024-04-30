100b4703242e3ebc89110151075d136152e83774
Nilay Jain <nilayjain12@gmail.com>
2024-04-29 19:18:59 -0700

    New commit
    New commit

--- Modify_Cover_Letter.bat
+++ Modify_Cover_Letter.bat
@@ -1,6 +1,5 @@
 @echo off
 set /p position_name=Enter position name:
 set /p company_name=Enter company name:
-cd /d "C:\Users\njain\OneDrive - Cal State Fullerton\SPRING 2024\Nilay Jain Resume\Cover Letter"
-python Python_Script_Change_PositionCompanyName.py "%position_name%" "%company_name%"
+python "C:\Users\njain\OneDrive - Cal State Fullerton\SPRING 2024\Nilay Jain Resume\Cover Letter\Update-Cover-Letter-Fields-Using-Python\Update-Cover-Letter-Fields-Using-Python\Python_Script_Change_PositionCompanyName.py" "%position_name%" "%company_name%"
 pause
